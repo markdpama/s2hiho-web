a6de2664b5269a6a4b9b106a8a259e867a62a0ff
Mark <mhaark29@gmail.com>
2018-06-12 15:37:43 +0800

    remove circle notifications

--- webmin/resources/views/la/dashboard.blade.php
+++ webmin/resources/views/la/dashboard.blade.php
@@ -477,16 +477,18 @@
 
 @push('scripts')
 <script>
+/*
 (function($) {
 	$('body').pgNotification({
 		style: 'circle',
 		title: 'LaraAdmin',
-		message: "Welcome to LaraAdmin...",
+		message: "Welcome to S2-Hiho Digital",
 		position: "top-right",
 		timeout: 0,
 		type: "success",
 		thumbnail: '<img width="40" height="40" style="display: inline-block;" src="{{ Gravatar::fallback(asset('la-assets/img/user2-160x160.jpg'))->get(Auth::user()->email, 'default') }}" data-src="assets/img/profiles/avatar.jpg" data-src-retina="assets/img/profiles/avatar2x.jpg" alt="">'
 	}).show();
 })(window.jQuery);
+*/
 </script>
 @endpush
